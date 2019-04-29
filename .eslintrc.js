f4fd1b96e7259657ac89a61e6aed14a0bebbea65
ariel tamayo <aj@atamayo.io>
2019-04-29 15:30:55 +0800

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": "airbnb",
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "react/jsx-uses-react": 1,
+    "react/jsx-uses-vars": [2],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
